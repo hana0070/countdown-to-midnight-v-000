1247c9032225afcb5822e004edc98484bfa1c0f9
HANA LEANDER <hana0070@gmail.com>
2019-03-26 02:55:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,8 +13,7 @@ def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     sleep(1)
-    seconds_to_midnight -= 1
-
+    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
